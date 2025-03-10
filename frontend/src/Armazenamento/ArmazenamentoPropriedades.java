e12e5177b3f1d66f7390a524c94c3fdf8188e759
Aluno <aluno@IPIRANGA.FATEC>
2025-03-10 20:17:04 -0300

    Inicio das alterações

---
+++ frontend/src/Armazenamento/ArmazenamentoPropriedades.java
@@ -0,0 +1,19 @@
+package Armazenamento;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class ArmazenamentoPropriedades {
+
+    private String localArmazenamento = "upload-dir";
+
+    public String getLocalArmazenamento() {
+        return localArmazenamento;
+    }
+
+    public void setLocalArmazenamento(String localArmazenamento) {
+        this.localArmazenamento = localArmazenamento;
+    }
+    
+}
